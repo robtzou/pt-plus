130d907cf0333d83aaa9b88727821dded374fe0a
Robert Tzou <bobbytzou@gmail.com>
2025-05-19 21:44:41 -0400

    Finalizing data structure

---
+++ summarizer/finalize.py
@@ -0,0 +1,7 @@
+"""
+The aim of this program is to finalize the professor profile to be displayed for the website.
+
+Attaches the following changes to profile:
+    top three courses taught/reviewed -- denoted as 'teaches'
+    avg_rating
+"""
